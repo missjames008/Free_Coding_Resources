b0d03550d35927af16dd8b6823971f419774ffe3
missjames008 <missjames008@gmail.com>
2017-08-22 22:08:45 -0700

    added modal functionality

---
+++ Free_Coding_Resources/js/boot_camp_modals.js
@@ -0,0 +1,18 @@
+//Get the modal for CodeYourFuture
+var modal1 = document.getElementById('myModal1');
+
+// Get the button that opens the modal
+var btn1 = document.getElementById("myBtn1");
+//
+// // Get the <span> element that closes the modal
+var span1 = document.getElementsByClassName("close1")[0];
+
+// When the user clicks on the button, open the modal
+btn1.onclick = function() {
+    modal1.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span1.onclick = function() {
+    modal1.style.display = "none";
+}
