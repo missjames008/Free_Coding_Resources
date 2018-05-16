9315494a0fced4446d3237ee750b77c6caf2e988
missjames008 <missjames008@gmail.com>
2018-05-16 11:44:51 -0700

    Added more modals

--- Free_Coding_Resources/js/online_resources_modals.js
+++ Free_Coding_Resources/js/online_resources_modals.js
@@ -51,3 +51,165 @@ onlinebtn3.onclick = function() {
 onlinespan3.onclick = function() {
     onlinemodal3.style.display = "none";
 }
+
+//Get the modal for Khan Academy
+var onlinemodal4 = document.getElementById('onlineModal4');
+
+// Get the button that opens the modal
+var onlinebtn4 = document.getElementById("onlineBtn4");
+//
+// // Get the <span> element that closes the modal
+var onlinespan4 = document.getElementsByClassName("onlineclose4")[0];
+
+// When the user clicks on the button, open the modal
+onlinebtn4.onclick = function () {
+    onlinemodal4.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+onlinespan4.onclick = function () {
+    onlinemodal4.style.display = "none";
+}
+
+//Get the modal for MIT
+var onlinemodal5 = document.getElementById('onlineModal5');
+
+// Get the button that opens the modal
+var onlinebtn5 = document.getElementById("onlineBtn5");
+//
+// // Get the <span> element that closes the modal
+var onlinespan5 = document.getElementsByClassName("onlineclose5")[0];
+
+// When the user clicks on the button, open the modal
+onlinebtn5.onclick = function () {
+    onlinemodal5.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+onlinespan5.onclick = function () {
+    onlinemodal5.style.display = "none";
+}
+
+//Get the modal for Skillcrush
+var onlinemodal6 = document.getElementById('onlineModal6');
+
+// Get the button that opens the modal
+var onlinebtn6 = document.getElementById("onlineBtn6");
+//
+// // Get the <span> element that closes the modal
+var onlinespan6 = document.getElementsByClassName("onlineclose6")[0];
+
+// When the user clicks on the button, open the modal
+onlinebtn6.onclick = function () {
+    onlinemodal6.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+onlinespan6.onclick = function () {
+    onlinemodal6.style.display = "none";
+}
+
+//Get the modal for Crunchzilla
+var onlinemodal7 = document.getElementById('onlineModal7');
+
+// Get the button that opens the modal
+var onlinebtn7 = document.getElementById("onlineBtn7");
+//
+// // Get the <span> element that closes the modal
+var onlinespan7 = document.getElementsByClassName("onlineclose7")[0];
+
+// When the user clicks on the button, open the modal
+onlinebtn7.onclick = function () {
+    onlinemodal7.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+onlinespan7.onclick = function () {
+    onlinemodal7.style.display = "none";
+}
+
+//Get the modal for Khan Kids
+var onlinemodal8 = document.getElementById('onlineModal8');
+
+// Get the button that opens the modal
+var onlinebtn8 = document.getElementById("onlineBtn8");
+//
+// // Get the <span> element that closes the modal
+var onlinespan8 = document.getElementsByClassName("onlineclose8")[0];
+
+// When the user clicks on the button, open the modal
+onlinebtn8.onclick = function () {
+    onlinemodal8.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+onlinespan8.onclick = function () {
+    onlinemodal8.style.display = "none";
+}
+
+//Get the modal for Scratch
+var onlinemodal9 = document.getElementById('onlineModal9');
+
+// Get the button that opens the modal
+var onlinebtn9 = document.getElementById("onlineBtn9");
+//
+// // Get the <span> element that closes the modal
+var onlinespan9 = document.getElementsByClassName("onlineclose9")[0];
+
+// When the user clicks on the button, open the modal
+onlinebtn9.onclick = function () {
+    onlinemodal9.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+onlinespan9.onclick = function () {
+    onlinemodal9.style.display = "none";
+}
+
+//Get the modal for Tynker
+var onlinemodal10 = document.getElementById('onlineModal10');
+
+// Get the button that opens the modal
+var onlinebtn10 = document.getElementById("onlineBtn10");
+//
+// // Get the <span> element that closes the modal
+var onlinespan10 = document.getElementsByClassName("onlineclose10")[0];
+
+// When the user clicks on the button, open the modal
+onlinebtn10.onclick = function () {
+    onlinemodal10.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+onlinespan10.onclick = function () {
+    onlinemodal10.style.display = "none";
+}
+
+//Get the modal for Kodable
+var onlinemodal11 = document.getElementById('onlineModal11');
+
+// Get the button that opens the modal
+var onlinebtn11 = document.getElementById("onlineBtn11");
+//
+// // Get the <span> element that closes the modal
+var onlinespan11 = document.getElementsByClassName("onlineclose11")[0];
+
+// When the user clicks on the button, open the modal
+onlinebtn11.onclick = function () {
+    onlinemodal11.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+onlinespan11.onclick = function () {
+    onlinemodal11.style.display = "none";
+}
+
+//Get the modal for edX
+var onlinemodal12 = document.getElementById('onlineModal12');
+
+// Get the button that opens the modal
+var onlinebtn12 = document.getElementById("onlineBtn12");
+//
+// // Get the <span> element that closes the modal
+var onlinespan12 = document.getElementsByClassName("onlineclose12")[0];
+
+// When the user clicks on the button, open the modal
+onlinebtn12.onclick = function () {
+    onlinemodal12.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+onlinespan12.onclick = function () {
+    onlinemodal12.style.display = "none";
+}
